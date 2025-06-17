de417e5a61c45de943f20cd594f4084e9a6ff142
ktexucoder <amangkantee@gmail.com>
2025-06-17 12:11:30 +0700

    Add files via upload

---
+++ client/cl_functions.lua
@@ -0,0 +1,129 @@
+local isTimerInstance = false
+local isTimerRunning = false -- flag variable to control the Timer thread
+
+function setUiShow(bool)
+    SendNUIMessage({
+        type = "show",
+        show = bool
+    })
+end
+
+function updateUiTime(timeLeft)
+    local days = math.floor(timeLeft / (24 * 60))
+    local hours = math.floor((timeLeft % (24 * 60)) / 60)
+    local minutes = timeLeft % 60
+
+    SendNUIMessage({
+        type = "update",
+        days = days,
+        hours = hours,
+        minutes = minutes
+    })
+end
+
+
+function updateTimeToDatabase(overrideTime)
+    TriggerServerEvent("knxr-antitroll:updateTime", overrideTime or timeLeft)
+end
+
+function startTimer()
+    local interval = timeToSafe
+
+    updateTimeToDatabase()
+
+    CreateThread(function()
+        isTimerInstance = true
+        isTimerRunning = true -- start the Timer thread
+        while hasProtection do
+            if interval == timeToSafe or timeLeft <= 0 then
+                interval = 0
+                setUiShow(true)
+                updateTimeToDatabase()
+            end
+
+            interval = interval + 1
+
+            if timeLeft <= 0 then
+                stopAntiTroll()
+                isTimerInstance = false
+                return
+            end
+
+            timeLeft = timeLeft - 1
+
+            updateUiTime(timeLeft)
+            Wait(1000 * 60)
+        end
+        isTimerRunning = false -- stop the Timer thread
+    end)
+end
+
+function startAntiTroll()
+    if not isTimerInstance then
+        startTimer()
+    end
+    setUiShow(true)
+
+    -- Anti VDM
+    if Config.DisableVDM then
+        SetWeaponDamageModifier(-1553120962, 0.0)
+    end
+
+    -- Anti Driveby
+    if not Config.DriveBy then
+        SetPlayerCanDoDriveBy(PlayerId(), false)
+    end
+
+    if Config.DisablePunching then
+        CreateThread(function()
+            while hasProtection do
+                Wait(5)
+                DisableControlAction(0, 140, true)
+                DisableControlAction(0, 141, true)
+                DisableControlAction(0, 142, true)
+            end
+        end)
+    end
+
+    if Config.DisableShooting then
+        CreateThread(function()
+            while hasProtection do
+                Wait(5)
+                DisablePlayerFiring(player, true)
+            end
+        end)
+    end
+
+    if Config.DisablePunchingDamage then
+        SetWeaponDamageModifier(-1569615261, 0.0)
+    end
+end
+
+function stopAntiTroll()
+    hasProtection = false
+    timeLeft = 0
+
+    isTimerInstance = false
+
+    -- Anti VDM
+    if Config.DisableVDM then
+        SetWeaponDamageModifier(-1553120962, 1.0)
+    end
+
+    -- Anti Driveby
+    if not Config.DriveBy then
+        SetPlayerCanDoDriveBy(PlayerId(), true)
+    end
+
+    if Config.DisablePunching then
+        EnableControlAction(0, 140, true)
+        EnableControlAction(0, 141, true)
+        EnableControlAction(0, 142, true)
+    end
+
+    if Config.DisablePunchingDamage then
+        SetWeaponDamageModifier(-1569615261, 1.0)
+    end
+    setUiShow(false)
+    isTimerRunning = false -- stop the Timer thread
+end
---
+++ client/cl_main.lua
@@ -0,0 +1,41 @@
+hasProtection = false
+timeLeft = 0
+
+-- STATIC VARIABLES
+timeToSafe = Config.TimeToSave
+
+RegisterNetEvent("knxr-antitroll:toggle", function(toggle, timeOverride)
+    hasProtection = toggle or not hasProtection
+    timeLeft = timeOverride or Config.HowLong
+
+    if hasProtection then
+        startAntiTroll()
+        updateTimeToDatabase(timeLeft)
+    else
+        stopAntiTroll()
+        updateTimeToDatabase(0)
+    end
+end)
+
+
+if Config.Framework == "ESX" then
+    RegisterNetEvent("esx:playerLoaded")
+    AddEventHandler("esx:playerLoaded", function(xPlayer, isNew, skin)
+        TriggerServerEvent("knxr-antitroll:onjoin", isNew)
+    end)
+
+    AddEventHandler("onResourceStart", function(resourceName)
+        if resourceName == GetCurrentResourceName() then
+            TriggerServerEvent("knxr-antitroll:onjoin", true)
+        end
+    end)
+elseif Config.Framework == "QBCore" then
+    RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
+    AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
+        TriggerServerEvent("knxr-antitroll:onjoin", true)
+    end)
+    RegisterNetEvent("QBCore:Client:OnPlayerUnload")
+    AddEventHandler("QBCore:Client:OnPlayerUnload", function()
+        stopAntiTroll()
+    end)
+end
